6306b8ee6125a0ca10c3cbd76e24d888497dd9f9
jiovanysoliman <80723892+jiovanysoliman@users.noreply.github.com>
2023-10-06 10:27:58 -0700

    Cleaned up the files extensively

--- Lab 1/lab1/DE1_SoC.sv
+++ Lab 1/lab1/DE1_SoC.sv
@@ -1,67 +1,54 @@
 /* Top-level module for LandsLand hardware connections to implement the parking lot system.*/
 
-module DE1_SoC (CLOCK_50, LEDR, SW, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5);
+module DE1_SoC (CLOCK_50, V_GPIO, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5);
 
-	input  logic		 CLOCK_50;	// 50MHz clock
-	input logic [2:0] SW;
-    output logic [2:0] LEDR;
+	input logic CLOCK_50;	// 50MHz clock
 	output logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;	// active low
-// 	inout  logic [35:0] V_GPIO;	// expansion header 0 (LabsLand board)
-	       logic inner, outer;
-	    logic [31:0] div_clk;
-		logic reset;
+	inout logic [35:0] V_GPIO;	// expansion header 0 (LabsLand board)
+	logic inner, outer, reset;
 	
+	// LED ON (1) = SENSOR BLOCKED. LED OFF (0) = SENSOR UNBLOCKED		 
+ 	// assign V_GPIO[35] (outer LED) to V_GPIO[24] (outer switch)
+ 	assign V_GPIO[32] = V_GPIO[24];
 	
-	parameter whichClock = 12; // 0.75 Hz clock
-   clock_divider cdiv (.clock(CLOCK_50),
-                                .reset(reset),
-                                .divided_clocks(div_clk));
-
-    // Clock selection; allows for easy switching between simulation and board clocks
-    logic clkSelect;
-    // Uncomment ONE of the following two lines depending on intention
-
-    // assign clkSelect = CLOCK_50; // for simulation
-    assign clkSelect = div_clk[whichClock]; // for board
-
+ 	// assign V_GPIO[33] (inner LED) to V_GPIO[26] (inner switch)
+ 	assign V_GPIO[34] = V_GPIO[29];
 	
-    // Turn off all 7-seg displays (active low: 1 = off, 0 = on)
-// 	assign HEX0 = 7'b1111111;
-// 	assign HEX1 = 7'b1111111;
-// 	assign HEX2 = 7'b1111111;
-// 	assign HEX3 = 7'b1111111;
-// 	assign HEX4 = 7'b1111111;
-// 	assign HEX5 = 7'b1111111;
+ 	// assign v_GPIO[35] (reset LED) to V_GPIO[23] (reset switch)
+ 	assign V_GPIO[35] = V_GPIO[23];
+ 	assign reset = V_GPIO[23];
 	
-	
-// 	// LED ON (1) = SENSOR BLOCKED. LED OFF (0) = SENSOR UNBLOCKED		 
-// 	// assign V_GPIO[35] (outer LED) to V_GPIO[24] (outer switch)
-// 	assign V_GPIO[32] = V_GPIO[24];
-	
-// 	// assign V_GPIO[33] (inner LED) to V_GPIO[26] (inner switch)
-// 	assign V_GPIO[34] = V_GPIO[29];
-	
-// 	// assign v_GPIO[35] (reset LED) to V_GPIO[23] (reset switch)
-// 	assign V_GPIO[35] = V_GPIO[23];
-// 	assign reset = V_GPIO[23];
-
-    assign LEDR[2] = SW[2]; // reset
-    assign LEDR[1] = SW[1]; // inner
-    assign LEDR[0] = SW[0]; // outer
-    assign reset   = SW[2];
-	
-	//2 DFFs in series for each input (switch) will help eliminate metastability
-	D_FF outer_switch_1 (.q(outer_wire), .d(SW[0]), .clk(clkSelect));
-	D_FF outer_switch_2 (.q(outer), .d(outer_wire), .clk(clkSelect));
-	
-	D_FF inner_switch_1 (.q(inner_wire), .d(SW[1]), .clk(clkSelect));
-	D_FF inner_switch_2 (.q(inner), .d(inner_wire), .clk(clkSelect));
-	
-	parking_lot_occupancy parking (.clk(clkSelect), .reset(reset), .inner(inner), .outer(outer), .HEX0(HEX0), .HEX1(HEX1), .HEX2(HEX2), .HEX3(HEX3), .HEX4(HEX4), .HEX5(HEX5));
+	parking_lot_occupancy parking (.clk(CLOCK_50), .reset(reset), .inner(inner), .outer(outer), .HEX0(HEX0), .HEX1(HEX1), .HEX2(HEX2), .HEX3(HEX3), .HEX4(HEX4), .HEX5(HEX5));
 
 endmodule  // DE1_SoC
 
-// change clock select for test bench for modelsim
-// basically copy car_detection
-// chance clock select back for uploading to FPGA
 
+
+///* Test bench for the GPIO example module */
+//module DE1_SoC_tb ();
+//
+//	// inout pins must be connected to a wire type
+//	wire [35:0] V_GPIO;
+//
+//	// additional logic required to simulate inout pins
+//	logic [35:0] V_GPIO_in;
+//	logic [35:0] V_GPIO_dir; // 1 = input, 0 = output
+//	
+//	// set up tristate buffers for inout pins
+//	genvar i;
+//	generate
+//		for (i = 0; i < 36; i++) begin : gpio
+//		assign V_GPIO[i] = V_GPIO_dir[i] ? V_GPIO_in[i] : 1'bZ;
+//		end
+//	endgenerate
+//	
+//	GPIO_example dut (.V_GPIO);
+//	initial begin
+//	// you only need to set the pin directions once
+//	V_GPIO_dir[24] = 1'b1;
+//	V_GPIO_dir[35] = 1'b0;
+//	// manipulate the V_GPIO input bits indirectly through V_GPIO_in
+//	V_GPIO_in[24] = 1'b1; #50;
+//	V_GPIO_in[24] = 1'b0; #50;
+//	end
+//endmodule // GPIO_example_tb
--- Lab 1/lab1/parking_lot_occupancy.sv
+++ Lab 1/lab1/parking_lot_occupancy.sv
@@ -2,38 +2,12 @@
 // May need to change module header with appropriate I/O as necessary.
 module parking_lot_occupancy(clk, reset, inner, outer, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5);
 	input  logic clk, reset, inner, outer;
-//	input  logic inner_switch, outer_switch; // off-board switches to mimic photosensor inputs
-//	output logic inner_led, outer_led; // off-board LEDs to indicate the value of photosensor inputs (1 = on/blocked, 0 = off/unblocked)
 	output logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5; // 7-bit counter for HEX display module
-// 	input  logic [35:0] V_GPIO;
-			 logic enter, exit, outer_wire, inner_wire;
+	logic enter, exit;
 			 
 
 	// connect car detector, car counter, and HEX display modules
 	car_detection detector (.clk(clk), .reset(reset), .outer(outer), .inner(inner), .enter(enter), .exit(exit)); 
 	car_counter counter    (.clk(clk), .reset(reset), .incr(enter), .decr(exit), .HEX0(HEX0), .HEX1(HEX1), .HEX2(HEX2), .HEX3(HEX3), .HEX4(HEX4), .HEX5(HEX5));
 
-	// output logic [4:0] counter; // 5-bit counter for HEX display module
-	// output logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
-	// inout  logic V_GPIO;
-	// 		 logic enter, exit, outer_wire, inner_wire, outer, inner;
-			 
-	// // LED ON (1) = SENSOR BLOCKED. LED OFF (0) = SENSOR UNBLOCKED		 
-	// // assign V_GPIO[35] (outer LED) to V_GPIO[28] (outer switch)
-	// assign V_GPIO[35] = V_GPIO[28];
-	
-	// // assign V_GPIO[33] (inner LED) to V_GPIO[30] (inner switch)
-	// assign V_GPIO[33] = V_GPIO[30];
-	
-	// //2 DFFs in series for each input (switch) will help eliminate metastability
-	// D_FF outer_switch_1 (.q(outer_wire), .d(V_GPIO[28]), .clk(clk));
-	// D_FF outer_switch_2 (.q(outer),      .d(outer_wire), .clk(clk));
-	
-	// D_FF inner_switch_1 (.q(inner_wire), .d(V_GPIO[30]), .clk(clk));
-	// D_FF inner_switch_2 (.q(inner),      .d(inner_wire), .clk(clk));
-	
-	// // connect car detector, car counter, and HEX display modules
-	// // (clk, reset, incr, decr, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5);
-	// car_detection detector (.clk(clk), .reset(reset), .outer(outer), .inner(inner), .enter(enter), .exit(exit)); 
-	// car_counter   counter  (.clk(clk), .reset(reset), .incr(enter), .decr(exit), .HEX0(HEX0), .HEX1(HEX1), .HEX2(HEX2), .HEX3(HEX3), .HEX4(HEX4), .HEX5(HEX5));
 endmodule
