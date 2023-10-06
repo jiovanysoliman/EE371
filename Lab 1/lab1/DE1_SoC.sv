fd2a5262037349b380e009c54dc3da6200b3453c
mexipino <130417249+mexipino@users.noreply.github.com>
2023-10-06 15:03:31 -0700

    nothing
    garbage stuff we dont need

--- Lab 1/lab1/DE1_SoC.sv
+++ Lab 1/lab1/DE1_SoC.sv
@@ -1,4 +1,7 @@
 /* Top-level module for LandsLand hardware connections to implement the parking lot system.*/
+//
+//// setting up the clock for the sim
+//	`timescale 1ns / 1ns;
 
 module DE1_SoC (CLOCK_50, V_GPIO, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5);
 
@@ -22,92 +25,101 @@ endmodule  // DE1_SoC
 
 
 
-///* Test bench for the GPIO example module */
-//module DE1_SoC_tb ();
-//
-//	// inout pins must be connected to a wire type
-//	wire [35:0] V_GPIO;
-//
-//	// additional logic required to simulate inout pins
-//	logic [35:0] V_GPIO_in;
-//	logic [35:0] V_GPIO_dir; // 1 = input, 0 = output
-//	
-//	// set up tristate buffers for inout pins
-//	genvar i;
-//	generate
-//		for (i = 0; i < 36; i++) begin : gpio
-//			assign V_GPIO[i] = V_GPIO_dir[i] ? V_GPIO_in[i] : 1'bZ;
-//		end
-//	endgenerate
-//	
-//	GPIO_example dut (.V_GPIO);
-//	
-//	initial begin
-//	// setting up the clock for the sim
-//	'timescale' 1ns / 1ns;
+/* Test bench for the GPIO example module */
+module DE1_SoC_tb ();
+
+	// inout pins must be connected to a wire type
+	wire [35:0] V_GPIO;
+
+	// additional logic required to simulate inout pins
+	logic [35:0] V_GPIO_in;
+	logic [35:0] V_GPIO_dir; // 1 = input, 0 = output
+	logic CLOCK_50;
+	logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
+	
+	
+	// set up tristate buffers for inout pins
+	genvar i;
+	generate
+		for (i = 0; i < 36; i++) begin : gpio
+			assign V_GPIO[i] = V_GPIO_dir[i] ? V_GPIO_in[i] : 1'bZ;
+		end
+	endgenerate
+	
+	DE1_SoC dut (.V_GPIO);	
+	
 //	parameter CLOCK_PERIOD = 100;
 //	initial begin
 //		clk <= 0;
 //		forever #(CLOCK_PERIOD/2) clk <= ~clk; // forever toggle the clock
 //	end
+
+	// Set up a simulated clock.
+    parameter CLOCK_PERIOD=100;
+    initial begin
+        CLOCK_50 <= 0;
+        forever #(CLOCK_PERIOD/2) CLOCK_50 <= ~CLOCK_50; // Forever toggle the clock
+    end
 	
-//	// you only need to set the pin directions once
-//	V_GPIO_dir[23] = 1'b1;
-//	V_GPIO_dir[24] = 1'b1;
-//	V_GPIO_dir[29] = 1'b1;
-//	
-//	V_GPIO_dir[32] = 1'b0;
-//	V_GPIO_dir[34] = 1'b0;
-//	V_GPIO_dir[35] = 1'b0;
-//	
-//	// manipulate the V_GPIO input bits indirectly through V_GPIO_in
-//	// activate reset switch once.
-//	V_GPIO_in[23] = 1'b1; #10;
-//	V_GPIO_in[23] = 1'b0; #10;
-//	
-//	// car going in sequence. NEEDS TO REPEAT 16 Times.
-//	V_GPIO_in[24] = 1'b1; #10;
-//	V_GPIO_in[29] = 1'b1; #10;
-//	V_GPIO_in[24] = 1'b0; #10;
-//	V_GPIO_in[29] = 1'b0; #10;
-//	
-//	// activate reset switch once.
-//	V_GPIO_in[23] = 1'b1; #10;
-//	V_GPIO_in[23] = 1'b0; #10;
-//	
-//	// car going in sequence. NEEDS TO REPEAT 16 Times.
-//	V_GPIO_in[24] = 1'b1; #10;
-//	V_GPIO_in[29] = 1'b1; #10;
-//	V_GPIO_in[24] = 1'b0; #10;
-//	V_GPIO_in[29] = 1'b0; #10;
-//	
-//	// car going out in sequence. NEEDS TO REPEAT 16 Times.
-//	V_GPIO_in[29] = 1'b1; #10;
-//	V_GPIO_in[24] = 1'b1; #10;
-//	V_GPIO_in[29] = 1'b0; #10;
-//	V_GPIO_in[24] = 1'b0; #10;
-//	
-//	// Car going in, then pedestrian going in. 
-//	V_GPIO_in[24] = 1'b1; #10;
-//	V_GPIO_in[29] = 1'b1; #10;
-//	V_GPIO_in[24] = 1'b0; #10;
-//	V_GPIO_in[29] = 1'b0; #10;
-//	
-//	V_GPIO_in[24] = 1'b1; #10;
-//	V_GPIO_in[24] = 1'b0; #10;
-//	V_GPIO_in[29] = 1'b1; #10;
-//	V_GPIO_in[29] = 1'b0; #10;
-//	
-//	// Car going out, then pedestrian going out. 
-//	V_GPIO_in[29] = 1'b1; #10;
-//	V_GPIO_in[24] = 1'b1; #10;
-//	V_GPIO_in[29] = 1'b0; #10;
-//	V_GPIO_in[24] = 1'b0; #10;
-//	
-//	V_GPIO_in[29] = 1'b1; #10;
-//	V_GPIO_in[29] = 1'b0; #10;
-//	V_GPIO_in[24] = 1'b1; #10;
-//	V_GPIO_in[24] = 1'b0; #10;
-//	
-//	end
-//endmodule // GPIO_example_tb
+	initial begin
+	
+		// you only need to set the pin directions once
+		V_GPIO_dir[23] = 1'b1;
+		V_GPIO_dir[24] = 1'b1;
+		V_GPIO_dir[29] = 1'b1;
+		
+		V_GPIO_dir[32] = 1'b0;
+		V_GPIO_dir[34] = 1'b0;
+		V_GPIO_dir[35] = 1'b0;
+		
+		// manipulate the V_GPIO input bits indirectly through V_GPIO_in
+		// activate reset switch once.
+		V_GPIO_in[23] = 1'b1; #10; @(posedge CLOCK_50);
+		V_GPIO_in[23] = 1'b0; #10;
+		
+		// car going in sequence. NEEDS TO REPEAT 16 Times.
+		V_GPIO_in[24] = 1'b1; #10;
+		V_GPIO_in[29] = 1'b1; #10;
+		V_GPIO_in[24] = 1'b0; #10;
+		V_GPIO_in[29] = 1'b0; #10;
+		
+		// activate reset switch once.
+		V_GPIO_in[23] = 1'b1; #10;
+		V_GPIO_in[23] = 1'b0; #10;
+		
+		// car going in sequence. NEEDS TO REPEAT 16 Times.
+		V_GPIO_in[24] = 1'b1; #10;
+		V_GPIO_in[29] = 1'b1; #10;
+		V_GPIO_in[24] = 1'b0; #10;
+		V_GPIO_in[29] = 1'b0; #10;
+		
+		// car going out in sequence. NEEDS TO REPEAT 16 Times.
+		V_GPIO_in[29] = 1'b1; #10;
+		V_GPIO_in[24] = 1'b1; #10;
+		V_GPIO_in[29] = 1'b0; #10;
+		V_GPIO_in[24] = 1'b0; #10;
+		
+		// Car going in, then pedestrian going in. 
+		V_GPIO_in[24] = 1'b1; #10;
+		V_GPIO_in[29] = 1'b1; #10;
+		V_GPIO_in[24] = 1'b0; #10;
+		V_GPIO_in[29] = 1'b0; #10;
+		
+		V_GPIO_in[24] = 1'b1; #10;
+		V_GPIO_in[24] = 1'b0; #10;
+		V_GPIO_in[29] = 1'b1; #10;
+		V_GPIO_in[29] = 1'b0; #10;
+		
+		// Car going out, then pedestrian going out. 
+		V_GPIO_in[29] = 1'b1; #10;
+		V_GPIO_in[24] = 1'b1; #10;
+		V_GPIO_in[29] = 1'b0; #10;
+		V_GPIO_in[24] = 1'b0; #10;
+		
+		V_GPIO_in[29] = 1'b1; #10;
+		V_GPIO_in[29] = 1'b0; #10;
+		V_GPIO_in[24] = 1'b1; #10;
+		V_GPIO_in[24] = 1'b0; #10;
+	
+	end
+endmodule // GPIO_example_tb
