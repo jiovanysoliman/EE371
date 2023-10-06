f1207ae06524049a3e383ba22622c3ea0d287fbc
mexipino <130417249+mexipino@users.noreply.github.com>
2023-10-06 00:04:04 -0700

    detection and occupancy
    car detection fully debugged. parking lot occupancy is a work in progress still but already incorporates series DFFs to help with metastability from the inputs. I added the DFF.sv file

--- Lab 1/lab1/car_detection.sv
+++ Lab 1/lab1/car_detection.sv
@@ -6,54 +6,122 @@ module car_detection (clk, reset, outer, inner, enter, exit);
 	output logic enter, exit;  // outputs connected to off-board LEDs in parking_lot_occupancy module
 
 	// enumerate the possible values for present state (ps), and next state (ns)
-	// S0: both unblocked
-	// S1: only inner blocked
-	// S2: both blocked
-	// s3: only outer blocked
-	// S4: restart sequence
-	enum {S0, S1, S2, S3, S4} ps, ns; 
+	enum {S0, S1, S2, S3, S4, S5, S6, S7} ps, ns; 
 	
 	// case transition logic
 	always_comb begin
 	
-		case (ps):
+		case (ps)
 		
-			S0: if (~outer && inner) ns = S1;
-				else if (outer && ~inner) ns = S3;
-				else if (outer && inner) ns = S4;
-				else ns = ps;
+			S0: begin // initial state
+					if(~outer & ~inner) ns = S1;
+					else					  ns = ps;
+				 end
 			
-			S1: if (outer && inner) ns = S2;
-				else if (outer && ~inner) ns = S4;
-				else if (~outer && ~inner) ns = S1;
-				else ns = ps;
+			S1: begin // both unblocked
+					if (outer & inner)   	  ns = S0;
+					else if (outer & ~inner)  ns = S2;
+					else if (~outer & ~inner) ns = ps;
+					else 							  ns = S3;
+				 end
 				
-			S2: if (outer && ~inner) ns = S3;
-				else if (~outer && ~inner) ns = S4;
-				else if (~outer && inner) ns = S1;
-				else ns = ps;
+			S2: begin // outer blocked
+					if (outer & ~inner) 		  ns = ps;
+					else if (~outer & ~inner) ns = S1;
+					else if (~outer & inner)  ns = S0;
+					else 							  ns = S4;
+				 end
 				
-			S3: if (~outer && inner) ns = S4;
-				else if (~outer && ~inner) ns = S0;
-				else if (outer && inner) ns = S2;
-				else ns = ps;
+			S3: begin // inner blocked
+					if (~outer & inner)       ns = ps;
+					else if (~outer & ~inner) ns = S1;
+					else if (outer & inner)   ns = S7;
+					else                      ns = S0;
+				 end
 			
+			S4: begin // both blocked (entering)
+					if (~outer & ~inner)      ns = S1;
+					else if (~outer & inner)  ns = S5;
+					else if (outer & ~inner)  ns = S0;
+					else							  ns = ps;
+				 end
 			
-			S4: if (~outer && ~inner) ns = S0;
-				else ns = ps;
+			S5: begin // end of enter sequence
+					if (~outer & ~inner)      ns = S1;
+					else							  ns = S0;
+				 end
+			
+			S6: begin // end of exit sequence
+					if (~outer & ~inner)      ns = S1;
+					else							  ns = S0;
+				 end
+			
+			S7: begin // both blocked (exiting)
+					if (~outer & ~inner)      ns = S1;
+					else if (~outer & inner)  ns = S0;
+					else if (outer & ~inner)  ns = S6;
+					else							  ns = ps;
+				 end
+			
+			default: ns = S0;
 		
 		endcase
 	end
 	
 	// output logic
-	//assign enter =
-	//assign exit =
+	assign enter = (ps == S5);
+	assign exit  = (ps == S6);
 	
 	// next state logic
 	always_ff @(posedge clk) begin
 		
-		if (reset) ps <= S4;
-		else ps <= ns;
+		if (reset) ps <= S0;
+		else       ps <= ns;
 	
 	end
 endmodule // car_detection
+
+module car_detection_tb();
+	logic clk, reset, outer, inner, enter, exit;
+	
+	car_detection dut (.*);
+	
+	// Set up a simulated clock
+	parameter CLOCK_PERIOD = 100;
+	initial begin
+		clk <= 0;
+		forever #(CLOCK_PERIOD/2) clk <= ~clk; // forever toggle the clock
+	end
+	
+	// Set up the inputs to the design. Each line is a clock cycle.
+	// Inputs designed such that each transition between states is made at least once.
+	
+	initial begin
+	
+		reset <= 1;												 @(posedge clk);
+		reset <= 0; outer <= 0; inner <= 0; repeat(2) @(posedge clk); // S4 --> S0
+						            inner <= 1;	repeat(2) @(posedge clk); // start exit sequence, verify same state for multiple cycles
+						outer <= 1;          	repeat(2) @(posedge clk); 
+										inner <= 0;	repeat(2) @(posedge clk); 
+						outer <= 0;					repeat(2) @(posedge clk); 
+						outer <= 1;					          @(posedge clk); // start enter sequence
+										inner <= 1;				 @(posedge clk); 
+						outer <= 0;								 @(posedge clk); 
+										inner <= 0;				 @(posedge clk); 
+		reset <= 1;                       	          @(posedge clk); // test restart sequence, same state for multiple cycles
+		reset <= 0;             inner <= 1; repeat(2) @(posedge clk); 
+						outer <= 1;   			   repeat(2) @(posedge clk); 
+										inner <= 0; repeat(2) @(posedge clk); 
+						outer <= 0;								 @(posedge clk); // test for pedestrian (enter/exit signals unaffected)
+										inner <= 1; 			 @(posedge clk); 
+										inner <= 0;				 @(posedge clk); 
+						outer <= 1;								 @(posedge clk);
+						outer <= 0;								 @(posedge clk);
+						outer <= 1; inner <= 1;				 @(posedge clk); // back to initial state
+						outer <= 0; inner <= 0;				 @(posedge clk); // test breaking out of enter sequence
+						outer <= 1;								 @(posedge clk);
+										inner <= 1;				 @(posedge clk);
+										inner <= 0;	repeat(4) @(posedge clk); // EXIT = 1!!!!
+																	 $stop;
+	end
+endmodule
--- Lab 1/lab1/parking_lot_occupancy.sv
+++ Lab 1/lab1/parking_lot_occupancy.sv
@@ -1,14 +1,28 @@
 // Combines car_detection module and car_counter module to determine occupancy of parking lot.
 // May need to change module header with appropriate I/O as necessary.
-module parking_lot_occupancy(clk, reset, inner_switch, outer_switch, inner_led, outer_led, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5);
-	input  logic inner_switch, outer_switch; // off-board switches to mimic photosensor inputs
+module parking_lot_occupancy(clk, reset, counter, V_GPIO);
 	input  logic clk, reset;
-	output logic inner_led, outer_led; // off-board LEDs to indicate the value of photosensor inputs (1 = on/blocked, 0 = off/unblocked)
-	output logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;	// active low
-			 logic enter, exit;
+//	input  logic inner_switch, outer_switch; // off-board switches to mimic photosensor inputs
+//	output logic inner_led, outer_led; // off-board LEDs to indicate the value of photosensor inputs (1 = on/blocked, 0 = off/unblocked)
+	output logic [4:0] counter; // 5-bit counter for HEX display module
+	inout  logic V_GPIO;
+			 logic enter, exit, outer_wire, inner_wire, outer, inner;
+			 
+	// LED ON (1) = SENSOR BLOCKED. LED OFF (0) = SENSOR UNBLOCKED		 
+	// assign V_GPIO[35] (outer LED) to V_GPIO[24] (outer switch)
+	assign V_GPIO[35] = V_GPIO[24];
 	
-	// connect car detector and car counter modules, have HEX display module separate
+	// assign V_GPIO[33] (inner LED) to V_GPIO[26] (inner switch)
+	assign V_GPIO[33] = V_GPIO[26];
 	
-	car_detection detector (.clk(clk), .reset(reset), .outer(outer_switch), .inner(inner_switch), .enter(enter), .exit(exit)); 
-	// car_counter   counter  (....);
+	//2 DFFs in series for each input (switch) will help eliminate metastability
+	D_FF outer_switch_1 (.q(outer_wire), .d(V_GPIO[24]), .clk(clk));
+	D_FF outer_switch_2 (.q(outer),      .d(outer_wire), .clk(clk));
+	
+	D_FF inner_switch_1 (.q(inner_wire), .d(V_GPIO[26]), .clk(clk));
+	D_FF inner_switch_2 (.q(inner),      .d(inner_wire), .clk(clk));
+	
+	// connect car detector, car counter, and HEX display modules
+	car_detection detector (.clk(clk), .reset(reset), .outer(outer), .inner(inner), .enter(enter), .exit(exit)); 
+
 endmodule
