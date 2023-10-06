99beed21b30e71ddd5ce5bb16eb753263d55f5d4
jiovanysoliman <80723892+jiovanysoliman@users.noreply.github.com>
2023-10-06 07:34:10 -0700

    Edits to De1_SOC

--- Lab 1/lab1/DE1_SoC.sv
+++ Lab 1/lab1/DE1_SoC.sv
@@ -1,11 +1,24 @@
 /* Top-level module for LandsLand hardware connections to implement the parking lot system.*/
 
-module DE1_SoC (CLOCK_50, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, LEDR, V_GPIO);
+module DE1_SoC (CLOCK_50, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, V_GPIO);
 
 	input  logic		 CLOCK_50;	// 50MHz clock
 	output logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;	// active low
-	output logic [9:0] LEDR;
 	inout  logic [35:0] V_GPIO;	// expansion header 0 (LabsLand board)
+	
+	
+	parameter whichClock = 0; // 0.75 Hz clock
+   clock_divider cdiv (.clock(CLOCK_50),
+                                .reset(reset),
+                                .divided_clocks(div_clk));
+
+    // Clock selection; allows for easy switching between simulation and board clocks
+    logic clkSelect;
+    // Uncomment ONE of the following two lines depending on intention
+
+    // assign clkSelect = CLOCK_50; // for simulation
+    assign clkSelect = div_clk[whichClock]; // for board
+
 	
     // Turn off all 7-seg displays (active low: 1 = off, 0 = on)
 	assign HEX0 = 7'b1111111;
@@ -14,5 +27,19 @@ module DE1_SoC (CLOCK_50, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, LEDR, V_GPIO);
 	assign HEX3 = 7'b1111111;
 	assign HEX4 = 7'b1111111;
 	assign HEX5 = 7'b1111111;
+	
+	
+	// LED ON (1) = SENSOR BLOCKED. LED OFF (0) = SENSOR UNBLOCKED		 
+	// assign V_GPIO[35] (outer LED) to V_GPIO[24] (outer switch)
+	assign V_GPIO[32] = V_GPIO[24];
+	
+	// assign V_GPIO[33] (inner LED) to V_GPIO[26] (inner switch)
+	assign V_GPIO[34] = V_GPIO[29];
+	
+	// assign v_GPIO[35] (reset LED) to V_GPIO[23] (reset switch)
+	assign V_GPIO[35] = V_GPIO[23];
+	assign reset = V_GPIO[23];
+	
+	parking_lot_occupancy parking (.clk(clkSelect), .V_GPIO, .HEX0, .HEX1, .HEX2, .HEX3, .HEX4, .HEX5);
 
 endmodule  // DE1_SoC
--- Lab 1/lab1/parking_lot_occupancy.sv
+++ Lab 1/lab1/parking_lot_occupancy.sv
@@ -1,25 +1,14 @@
 // Combines car_detection module and car_counter module to determine occupancy of parking lot.
 // May need to change module header with appropriate I/O as necessary.
-module parking_lot_occupancy(clk, V_GPIO, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5);
-	input  logic clk;
+module parking_lot_occupancy(clk, reset, V_GPIO, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5);
+	input logic clk, reset;
+	input logic [35:0] V_GPIO;
 //	input  logic inner_switch, outer_switch; // off-board switches to mimic photosensor inputs
 //	output logic inner_led, outer_led; // off-board LEDs to indicate the value of photosensor inputs (1 = on/blocked, 0 = off/unblocked)
 	output logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5; // 7-bit counter for HEX display module
-	inout  logic [35:0] V_GPIO;
-			 logic enter, exit, outer_wire, inner_wire, outer, inner;
-			 
-	// LED ON (1) = SENSOR BLOCKED. LED OFF (0) = SENSOR UNBLOCKED		 
-	// assign V_GPIO[35] (outer LED) to V_GPIO[24] (outer switch)
-	assign V_GPIO[32] = V_GPIO[24];
-	
-	// assign V_GPIO[33] (inner LED) to V_GPIO[26] (inner switch)
-	assign V_GPIO[34] = V_GPIO[29];
-	
-	// assign v_GPIO[35] (reset LED) to V_GPIO[23] (reset switch)
-	assign V_GPIO[35] = V_GPIO[23];
-	assign reset = V_GPIO[23];
-	
-	
+	logic enter, exit, outer_wire, inner_wire, outer, inner;
+			 			 
+
 	//2 DFFs in series for each input (switch) will help eliminate metastability
 	D_FF outer_switch_1 (.q(V_GPIO[24]), .d(outer_wire), .clk);
 	D_FF outer_switch_2 (.q(outer_wire), .d(outer), .clk);
