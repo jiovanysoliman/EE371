f7b00d4f0944b014d586b95dc0e3945b825358e3
jiovanysoliman <80723892+jiovanysoliman@users.noreply.github.com>
2023-10-22 12:49:39 -0700

    Update task2_toplevel.sv

--- Lab 3/lab3/task2_toplevel.sv
+++ Lab 3/lab3/task2_toplevel.sv
@@ -1,6 +1,15 @@
-module task2_toplevel ()
+module task2_toplevel (CLOCK_50, SW)
+
+input logic CLOCK_50;
+input logic [9:0] SW;
+logic output1, output2;
+output logic topOutput;
+
+
 
 part1 task1 ();
 part2 task2 ();
+
+assign topOutput = SW[0] ? output2 : output1;
 
 endmodule
