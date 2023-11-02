133c61ad8eb64087d8db0b5b18300dcd4da1a1d7
jiovanysoliman <80723892+jiovanysoliman@users.noreply.github.com>
2023-11-02 13:35:18 -0700

    More edits for task 2

--- Lab 4/lab 4/DE1_SoC.sv
+++ Lab 4/lab 4/DE1_SoC.sv
@@ -1,30 +1,45 @@
-// Top level module for lab 4
-// Allows switching between task 1 and task 2 using SW[9]
+// Top-level module for LandsLand hardware connections to demonstrate Lab 4, task 1 and task 2 using DE1_SoC interfaces.*/
+// SW is a 10 bit input
+// KEY is a 4 bit input
+// HEX5- HEX0 is a 7 bit output.
+module DE1_SoC(CLOCK_50, SW, KEY, HEX5, HEX4, HEX3, HEX2, HEX1, HEX0);
+
+	input logic CLOCK_50;
+	input logic [9:0] SW;
+	input logic [3:0] KEY;
+	output logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;	// active low
+	logic temp1, temp2, Start, Reset, A;
+
+	//// switch between task 1 and task 2. 
+	//Assign algo = SW[9];
+	//
+	//always_comb begin
+	//
+	//	case(SW[9]) begin
+	//	
+	//		0: counter counterAlgo ();
+	//	
+	//		1: binary binaryAlgo ();
+	//		
+	//	endcase
+	//
+	//
+	//end
 
 
-module DE1_SoC(CLOCK_50, SW, HEX5, HEX4, HEX3, HEX2, HEX1, HEX0);
-
-
-//// switch between task 1 and task 2. 
-//Assign algo = SW[9];
+//	always_ff (posedge clk); begin
 //
-//always_comb begin
-//
-//	case(SW[9]) begin
-//	
-//		0: counter counterAlgo ();
-//	
-//		1: binary binaryAlgo ();
+//		temp1 <= KEY[3];
+//		Start <= temp1;
 //		
-//	endcase
+//		temp2 <= KEY[0];
+//		Reset <= temp2;
+//		
+////	end 
 //
-//
-//end
+//	assign A = SW[7:0];
 
-//assign Start = ~KEY[3];
-//assign Reset = ~KEY[0];
-
-//seg7 LocOutMSB (.hex(Loc[4]), .leds(HEX1));
-//seg7 LocOutLSB (.hex(Loc[3:0]), .leds(HEX0));
+	//seg7 LocOutMSB (.hex(Loc[4]), .leds(HEX1));
+	//seg7 LocOutLSB (.hex(Loc[3:0]), .leds(HEX0));
 
 endmodule 
--- Lab 4/lab 4/binary.sv
+++ Lab 4/lab 4/binary.sv
@@ -1,13 +1,15 @@
-module binary #(parameter A = 5) (CLOCK_50, SW, Loc, Done, Found);
+module binary #(parameter InputWidth = 8, parameter LocWidth = 5) (CLOCK_50, A, Start, Reset, Loc, Done, Found);
 
 input logic CLOCK_50;
-input logic [9:0] SW;
-output logic [4:0] Loc;
+input logic [InputWidth-1:0] A;
+input logic Start;
+input logic Reset;
+output logic [LocWidth:0] Loc;
 output logic Done;
 output logic Found;
 
 
-seg7 LocOutMSB (.hex(Loc[4]), .leds(HEX1));
-seg7 LocOutLSB (.hex(Loc[3:0]), .leds(HEX0));
 
-endmodule 
+
+
+endmodule 	
