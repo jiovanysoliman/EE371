474d97172600000b08ae1ba018abbd5588067eb4
jiovanysoliman <80723892+jiovanysoliman@users.noreply.github.com>
2023-11-02 13:47:16 -0700

    Task 2 Memory Part Done

--- Lab 4/lab 4/binary.sv
+++ Lab 4/lab 4/binary.sv
@@ -4,12 +4,14 @@ input logic CLOCK_50;
 input logic [InputWidth-1:0] A;
 input logic Start;
 input logic Reset;
-output logic [LocWidth:0] Loc;
+output logic [LocWidth-1:0] Loc;
 output logic Done;
 output logic Found;
+logic [LocWidth-1:0] address;
+logic [InputWidth-1:0] q;
 
 
+RAM_32_8_1port RAM (.address(address), .clock(CLOCK_50), .data(0), .wren(0), .q(q));
 
 
-
-endmodule 	
+endmodule
